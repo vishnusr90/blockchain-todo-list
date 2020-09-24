20e04b0bc87b2a0ca656f6d20b547c40aa0e36ee
Vishnu <vishnusr01@gmail.com>
2020-09-24 23:23:23 +0800

    Add more functions of Task contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity ^0.5.16;
 contract TodoList {
     // Keep track of number  of to do
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("My first task");
+        createTask("Exercise");
+    }
+    mapping(uint => Task) public tasks;
+
+    // to add task to mapping
+    function createTask(string memory content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, content, false);
+    }
 }
